e09c2778b8928fd3bb15de9014af7cbefc9e48d1
MMDDKK6500 <joao.dnwandermuren@senacsp.edu.br>
2026-04-17 21:34:54 -0300

    Adicionado percursos

--- AplicacaoArvoreBinaria/src/aplicacaoarvorebinaria/Principal.java
+++ AplicacaoArvoreBinaria/src/aplicacaoarvorebinaria/Principal.java
@@ -1,8 +1,27 @@
 package aplicacaoarvorebinaria;
 
+import arvorebinaria.ABB;
+
 public class Principal {
 
     public static void main(String[] args) {
+        
+        ABB arvore = new ABB();
+        
+        arvore.insert(50);
+        arvore.insert(20);
+        arvore.insert(10);
+        arvore.insert(100);
+        arvore.insert(200);
+        arvore.insert(1);
+        arvore.insert(25);
+        arvore.insert(8);
+        arvore.insert(150);
+        arvore.insert(500);
+        
+        arvore.printPrecurso();
+        arvore.printIncurso();
+        arvore.printPoscurso();
         
     }
     
--- AplicacaoArvoreBinaria/src/arvorebinaria/ABB.java
+++ AplicacaoArvoreBinaria/src/arvorebinaria/ABB.java
@@ -43,5 +43,53 @@ public class ABB {
         }
         return true;
     } 
+ 
+    public void printPrecurso() {
+        System.out.println("precurso: ");
+        precurso(this.raiz);
+        System.out.println("");
+    }
+    
+    private void precurso(No aux) {
+        System.out.printf("%.2f - ", aux.getValor());
+        if (aux.getEsquerda() != null) {
+            precurso(aux.getEsquerda());
+        }
+        if (aux.getDireita() != null){
+            precurso(aux.getDireita());
+        }
+    }
+    
+    public void printIncurso() {
+        System.out.println("incurso: ");
+        incurso(this.raiz);
+        System.out.println("");
+    }
+    
+    private void incurso(No aux) {
+        if (aux.getEsquerda() != null) {
+            incurso(aux.getEsquerda());
+        }
+        System.out.printf("%.2f - ", aux.getValor());
+        if (aux.getDireita() != null){
+            incurso(aux.getDireita());
+        }
+    }
+    
+    public void printPoscurso() {
+        System.out.println("incurso: ");
+        poscurso(this.raiz);
+        System.out.println("");
+    }
+    
+    private void poscurso(No aux) {
+        if (aux.getEsquerda() != null) {
+            poscurso(aux.getEsquerda());
+        }
+        if (aux.getDireita() != null){
+            poscurso(aux.getDireita());
+        }
+        System.out.printf("%.2f - ", aux.getValor());
+    }
     
 }
